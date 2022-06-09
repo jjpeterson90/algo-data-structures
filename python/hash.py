b340fb624a75ccdd5bd531de19d28bec0a4cb749
Justin <jjpeterson90@hotmail.com>
2022-06-09 18:22:31 -0500

    minor fixes

--- python/hash.py
+++ python/hash.py
@@ -2,23 +2,34 @@ class HashTable:
   def __init__(self, number_of_buckets):
     # self.number_of_buckets = number_of_buckets
     # self.table = [None] * self.number_of_buckets
+    self.number_of_buckets = 30
+    self.table = [None] * self.number_of_buckets
+    
     pass
 
-  def hash(self, value):
+  def hash(self, key):
     # here is where you'll turn your 'value' into a hash value that will return the index of your table to insert value
     # IMPORTANT: Think about how you'd store values into the same index
-    pass
+    hash = 0
+    for i in range(len(key)):
+      hash += key.charCodeAt(i)
+    return hash % len(self.table)
 
-  def set(self, value):
+  def set(self, key, value):
     # here is where you'll perform your logic to insert the value into your table
     # you'll also call your hash method here to get the index where you'll store the value
-    pass
+    index = self.hash(key)
+    self.table[index].append([key,value])
 
-  def get(self, value):
+  def get(self, key):
     # here is where you'll retreive your value from the hash table
     # IMPORTANT: Think about how you'd retreive a value that from an index that has multiple values
-    pass
+    index = self.hash(key)
+    for data in self.table[index]:
+      if data[0] == key:
+        return data[1]
 
   def has_key(self, value):
     # here is where you'll return a True or False value if there is a value stored at a specific index in your HashTable
+    
     pass
--- python/linked_list.py
+++ python/linked_list.py
@@ -1,34 +1,54 @@
-from operator import index
-import queue
-
 
 class LinkList:
   # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  
   def __init__(self):
-    self.nodes = []
-    self.previous_node = None
-    self.name = 0
     self.head = None
     self.length = 0
-    
-    pass
+
 
   def add(self, data):
     # How to change previous node's pointer when a new node is added?
-    self.nodes.append(Node(data))
+    if self.length == 0:
+      self.head = Node(data)
+      self.length += 1
+    else:
+      current_node = self.head
+    
+      while current_node.next != None:
+        current_node = current_node.next
 
-    self.length += 1
-    pass
+      current_node.next = Node(data)
+      self.length += 1
+
 
   def remove(self, data):
-    # write your code to REMOVE an element from the Linked List
+    previous_node = self.head
+    node_to_delete = self.get(data)
+    
+    while previous_node.next.data != data:
+      previous_node = previous_node.next
+    
+    if node_to_delete.next != None:
+      previous_node.next = node_to_delete.next
+    else:
+      del node_to_delete
     self.length -= 1
-    pass
+      
 
   def get(self, element_to_get):
     # write you code to GET and return an element from the Linked List
+    current_node = self.head
     
-    pass
+    while current_node.data != element_to_get:
+      current_node = current_node.next
+      print(current_node.data)
+      if current_node.next == None:
+        if current_node.data != element_to_get:
+          return "That node does not exist"
+        return current_node
+    return current_node
+
 
 # ----- Node ------
 class Node:
@@ -38,6 +58,20 @@ class Node:
     self.data = data
     self.next = None
   
-  pass
+
+
 
 List = LinkList()
+
+List.add('A')
+List.add('B')
+List.add('C')
+List.add('D')
+List.add('E')
+
+List.get('E')
+
+List.remove('D')
+print("\n")
+
+List.get('E')
